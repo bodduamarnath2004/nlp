6046ef5d1289aac2e111be079c2e5ebce7c1b472
bodduamarnath2004 <129674621+bodduamarnath2004@users.noreply.github.com>
2023-09-17 13:15:57 +0530

    Update app.py

--- nlp/app.py
+++ nlp/app.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import re
+pip install nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
